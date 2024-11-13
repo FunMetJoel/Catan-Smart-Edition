17c4c66276efdac4a00b850acc6f55eb6e2d2f1a
FunMetJoel <machinebouwclub@gmail.com>
2024-11-13 21:54:25 +0000

    Some server stuff

--- CatanServer/src/main.py
+++ CatanServer/src/main.py
@@ -2,13 +2,19 @@ import flask
 
 app = flask.Flask(__name__)
 
+users = []
+
 @app.route('/')
 def home():
     return "Welcome to Catan Smart Edition!"
 
 @app.route('/api/v1/players', methods=['GET'])
 def get_players():
-    return "List of players"
+    global users
+    ip = flask.request.remote_addr
+    users.append(ip)
+    return flask.jsonify(users)
+
 
 if __name__ == '__main__':
     app.run(
